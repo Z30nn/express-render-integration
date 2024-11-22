ae6a7d9a719cc6dca7897aefedc7eb7116a230df
Junno Co <junno.elizer@gmail.com>
2024-11-22 23:55:07 +0800

    Added cors

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const productService = require('./product-service');
+
+app.use(cors());
 
 const app = express();
 const port = process.env.PORT || 3000;
